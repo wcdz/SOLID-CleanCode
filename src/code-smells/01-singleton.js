9ffb3c02a45f7692679f0b3c2a731ec1fb02f36d
Hernan Matias Escalante Guerra <hescalante@pucp.edu.pe>
2023-08-08 11:14:13 -0500

    avance STUPID

---
+++ src/code-smells/01-singleton.js
@@ -0,0 +1,29 @@
+const Singleton = (function () {
+
+    let instance;
+
+    function createInstance() {
+        return new Object('I am the instance');
+    }
+
+    return {
+        getInstance() {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+function main() {
+
+    const instance1 = Singleton.getInstance();
+    const instance2 = Singleton.getInstance();
+
+    console.log('Misma instancia? ', (instance1 === instance2));
+    console.log(instance1);
+    console.log(instance2);
+}
+
+main();
